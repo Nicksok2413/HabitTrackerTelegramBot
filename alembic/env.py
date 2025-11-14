efd48f1bacd2af557013b45189991f83f0e7f327
nick <8650180292@my.lerna.by>
2025-11-14 13:50:09 +0300

    update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,13 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
+# Импортируем базовую модель SQLAlchemy
+from src.api.models.base import Base
+
+# # Импортируем все модели, чтобы они зарегистрировались в Base.metadata
+# from src.api.models import habit, habit_execution, user # type: ignore
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -13,11 +20,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# Указываем Alembic на метаданные базовой модели
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
