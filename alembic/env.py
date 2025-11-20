3c620cf24871dd74b84366b5c4b9222b130625c2
nick <8650180292@my.lerna.by>
2025-11-20 15:52:51 +0300

    update alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,8 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 
 # Импортируем базовую модель SQLAlchemy
-from src.api.models.base import Base
-# # Импортируем все модели, чтобы они зарегистрировались в Base.metadata
-# from src.api.models import habit, habit_execution, user # type: ignore
+from src.api.models import Base  # Это подтянет все модели через __init__
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
